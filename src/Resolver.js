c2eea3003166afcb527872dfefd92f8e6813d516
Mehedi Hassan <MehediDracula@gmail.com>
2019-01-25 18:03:51 +0600

    Fix style

--- src/Resolver.js
+++ src/Resolver.js
@@ -602,7 +602,7 @@ class Resolver {
         let currentPath = currentFile.substr(0, currentFile.lastIndexOf('/'));
         let composerFile = await vscode.workspace.findFiles('composer.json');
 
-        if (!composerFile.length) {
+        if (! composerFile.length) {
             return this.showErrorMessage('No composer.json file found, automatic namespace generation failed');
         }
 
@@ -611,15 +611,18 @@ class Resolver {
         vscode.workspace.openTextDocument(composerFile).then((document) => {
             let composerJson = JSON.parse(document.getText());
             let psr4 = composerJson.autoload['psr-4'];
+
             if (psr4 === undefined) {
                 return this.showErrorMessage('No psr-4 key in composer.json autoload object, automatic namespace generation failed');
             }
 
             let namespaceBase = Object.keys(psr4)[0];
             let baseDir = psr4[namespaceBase];
+
             namespaceBase = namespaceBase.replace(/\\/, '');
 
             let namespace = currentPath.split(baseDir);
+
             if (namespace[1]) {
                 namespace = namespace[1]
                 namespace = namespace.replace(/\//g, '\\');
@@ -628,6 +631,7 @@ class Resolver {
             } else {
                 namespace = namespaceBase;
             }
+
             namespace = 'namespace ' + namespace + ';' + "\n"
 
             this.activeEditor().edit(textEdit => {
